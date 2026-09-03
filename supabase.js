f8a7ec63cdd27bb3a08965017f9c57d542b01ce4
Fawaz Grafix <akoredefawaz49@gmail.com>
2026-09-03 21:26:43 +0100

    Change Supabase publishable key
    Updated the Supabase key to a new publishable key.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://aotxnbycfbvsgklvvusv.supabase.co";
 
-const SUPABASE_KEY = "YOUR_SB_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = "sb_publishable_vT2PtVMp0Y6G0GWH0ogjZA_wq2Qt_3u";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
